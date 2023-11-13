250792ef336632ce97b11e1e837f71fb56982b90
Ulf Svensson <ulf.e.sv@gmail.com>
2023-11-13 21:19:09 +0100

    update

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> fxrsoft</span>
+        <span> </span>
       </a>
     </span>
   )
